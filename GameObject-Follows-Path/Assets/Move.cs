8ca0a2d0204812e2d3a0fc53da8e8eace51ac23d
Jackie Ren <Jackie Ren>
2016-11-06 15:34:56 +0800

    Init!

---
+++ GameObject-Follows-Path/Assets/Move.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+    public Path path;
+    public float speed = 1f;
+    public bool isLoop = false;
+    public bool isReversed = false;
+
+    private int m_currentSegment;
+    private float m_transition;
+
+    void Start() {
+        if (isReversed) {
+            m_currentSegment = path.pointList.Length - 1;
+        } else {
+            m_currentSegment = 0;
+        }
+    }
+	
+	void Update () {
+        Play();
+    }
+
+    private void Play() {
+        m_transition += Time.deltaTime * speed;
+        if (m_transition > 1) {
+            m_transition = 0;
+            m_currentSegment = (isReversed) ? m_currentSegment - 1 : m_currentSegment + 1;
+        }
+        if (isReversed) {
+            if (m_currentSegment > 0) {
+                transform.position = path.LinearPosition(m_currentSegment, m_transition, false);
+                transform.rotation = path.Orientation(m_currentSegment, m_transition, false);
+            }
+            if (isLoop) {
+                m_currentSegment = path.pointList.Length - 1;
+            }
+        } else {
+            if (m_currentSegment < path.pointList.Length - 1) {
+                transform.position = path.LinearPosition(m_currentSegment, m_transition);
+                transform.rotation = path.Orientation(m_currentSegment, m_transition);
+            }
+            if (isLoop) {
+                m_currentSegment = 0;
+            }
+        }
+    }
+
+}
---
+++ GameObject-Follows-Path/Assets/Path.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Path : MonoBehaviour {
+
+    public Transform[] pointList;
+
+    private List<Vector3> m_pointPositionList = new List<Vector3>();
+    private List<Quaternion> m_pointRotationList = new List<Quaternion>();
+
+	void Start () {
+        m_pointPositionList.Clear();
+        m_pointRotationList.Clear();
+        for (int i = 0; i < pointList.Length; ++i) {
+            m_pointPositionList.Add(pointList[i].position);
+            m_pointRotationList.Add(pointList[i].rotation);
+        }
+	}
+	
+	void Update () {
+        DrawLine();
+    }
+
+    public Vector3 LinearPosition(int index, float ratio, bool isForward = true) {
+        Vector3 pos1 = m_pointPositionList[index];
+        Vector3 pos2 = (isForward) ? m_pointPositionList[index + 1] : m_pointPositionList[index - 1];
+        return Vector3.Lerp(pos1, pos2, ratio);
+    }
+
+    public Quaternion Orientation(int index, float ratio, bool isForward = true) {
+        Quaternion rot1 = m_pointRotationList[index];
+        Quaternion rot2 = (isForward) ? m_pointRotationList[index + 1] : m_pointRotationList[index - 1];
+        return Quaternion.Lerp(rot1, rot2, ratio);
+    }
+
+    private void DrawLine() {
+        for (int i = 0; i < pointList.Length - 1; ++i) {
+            Debug.DrawLine(pointList[i].position, pointList[i + 1].position, Color.blue);
+        }
+    }
+
+}
